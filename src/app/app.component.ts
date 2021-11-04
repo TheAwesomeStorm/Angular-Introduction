41e0e47a67bcc3c7ca246a2fe6291f74518d6f5a
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-03 23:21:02 -0300

    Added bootstrap and its dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-introduction';
+  url = 'https://www.freecodecamp.org/news/content/images/2019/07/panel-1-1.png'
+  description = 'JavaScript meme'
 }
