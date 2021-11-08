f13b80b84c51ff849f5a8430531387e63df931da
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 13:49:48 -0300

    PhotosComponents now implements OnChanges instead of OnInit

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core'
 import { Photo } from '../../photo/photo'
 
 @Component({
@@ -6,11 +6,22 @@ import { Photo } from '../../photo/photo'
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
   @Input() photos: Photo[] = []
+  rows: Photo[][] = [];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges (changes: SimpleChanges) {
+    if(changes['photos']) {
+      this.rows = this.groupColumns(this.photos)
+    }
   }
 
+  groupColumns(photos: Photo[]) {
+    const newRows: Photo[][] = [];
+    for(let index = 0; index < photos.length; index+=3) {
+      newRows.push(photos.slice(index, index + 3))
+    }
+    return newRows
+  }
 }
