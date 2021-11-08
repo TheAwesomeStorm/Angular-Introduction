79aa8d19df71760ed8cdb422c966cc558ddca173
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 13:26:14 -0300

    Added PhotosComponent inside PhotoListComponent

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,10 +3,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoComponent } from './photo/photo.component'
 import { PhotoListComponent } from './photo-list/photo-list.component'
 import { CommonModule } from '@angular/common';
-import { PhotoFormComponent } from './photo-form/photo-form.component'
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component'
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent ],
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent ],
   imports: [ HttpClientModule, CommonModule ]
 })
 export class PhotosModule {}
