31739e77c3fff0c58ef667fbbd8c53330b6533e4
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 14:45:21 -0300

    Added FilterByDescriptionPipe

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,10 +4,11 @@ import { PhotoComponent } from './photo/photo.component'
 import { PhotoListComponent } from './photo-list/photo-list.component'
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component'
+import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe'
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent ],
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionPipe ],
   imports: [ HttpClientModule, CommonModule ]
 })
 export class PhotosModule {}
