880cdc6bb4392a905f435084750a8ee10a75c0a2
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 10:48:54 -0300

    Added PhotoFormComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component, OnInit } from '@angular/core'
-import { Photo } from './photos/photo/photo'
-import { PhotoService } from './photos/photo/photo.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'AngularIntroduction'
-  photos: Photo[] = []
-  constructor (private photoService: PhotoService) {}
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio').subscribe(photos => this.photos = photos)
-  }
-}
+export class AppComponent {}
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Photo } from '../photo/photo'
+import { PhotoService } from '../photo/photo.service'
 
 @Component({
   selector: 'app-photo-list',
@@ -6,10 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-list.component.scss']
 })
 export class PhotoListComponent implements OnInit {
-
-  constructor() { }
-
+  photos: Photo[] = []
+  constructor (private photoService: PhotoService) {}
   ngOnInit(): void {
+    this.photoService.listFromUser('flavio').subscribe(photos => this.photos = photos)
   }
-
 }
