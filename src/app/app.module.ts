91dc04b8d99d732de16d6f04876ffb91e40a8cec
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 11:41:16 -0300

    Added ErrorModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module'
 import { AppRoutingModule } from './app.routing.module'
+import { ErrorsModule } from './errors/errors.module'
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppRoutingModule } from './app.routing.module'
   imports: [
     BrowserModule,
     PhotosModule,
+    ErrorsModule,
     AppRoutingModule
   ],
   providers: [],
