cf6735d0c8204d7a86fd3364fc04b8eb460fee9b
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 23:01:41 -0300

    Added method load to PhotoListComponent

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,10 +5,11 @@ import { PhotoListComponent } from './photo-list/photo-list.component'
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe'
+import { FilterByDescriptionPipe } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component'
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionPipe ],
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescriptionPipe, LoadButtonComponent ],
   imports: [ HttpClientModule, CommonModule ]
 })
 export class PhotosModule {}
