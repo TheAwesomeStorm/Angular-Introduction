de43ac93711cd209fcd502461475cce669b90bd4
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 22:36:28 -0300

    Added unsubscribe to debounce onDestroy

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Subject } from 'rxjs'
 import { debounceTime } from 'rxjs'
@@ -9,15 +9,24 @@ import { Photo } from '../photo/photo'
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = []
   filter: string = ''
   debounce: Subject<EventTarget | null> = new Subject<EventTarget | null>()
+
   constructor (private activatedRoute: ActivatedRoute) {}
+
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos']
-    this.debounce.pipe(debounceTime(300)).subscribe(eventTarget => this.onKeyUp(eventTarget))
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(eventTarget => this.onKeyUp(eventTarget))
   }
+
+  ngOnDestroy () {
+    this.debounce.unsubscribe()
+  }
+
   onKeyUp(target: EventTarget | null) {
     if(target instanceof EventTarget) {
       const element = target as HTMLInputElement
