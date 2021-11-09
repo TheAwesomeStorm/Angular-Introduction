dfd7903b4bf7fb622c14d4e58254cf19fd48db3b
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-08 22:45:21 -0300

    Added method listFromUserPaginated to PhotoService

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Photo } from './photo'
 import { Observable } from 'rxjs'
@@ -9,5 +9,10 @@ export class PhotoService {
 
   listFromUser(userName: string): Observable<Photo[]> {
     return this.http.get<Photo[]>(`http://localhost:3000/${userName}/photos`)
+  }
+
+  listFromUserPaginated(userName: string, page: number) {
+    const params = new HttpParams().append('page', page.toString())
+    return this.http.get<Photo[]>(`http://localhost:3000/${userName}/photos`, { params })
   }
 }
