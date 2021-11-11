43c192c9e04219bd5265a26346b904ffd6e76417
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-11 10:24:49 -0300

    Removed properties and methods from PhotoListComponent that were refactored to SearchComponent

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { Subject } from 'rxjs'
-import { debounceTime } from 'rxjs'
 import { Photo } from '../photo/photo'
 import { PhotoService } from '../photo/photo.service'
 
@@ -10,10 +8,9 @@ import { PhotoService } from '../photo/photo.service'
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   photos: Photo[] = []
   filter: string = ''
-  debounce: Subject<EventTarget | null> = new Subject<EventTarget | null>()
   hasMore: boolean = true
   currentPage: number = 1
   userName: string = ''
@@ -25,20 +22,6 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params['userName']
     this.photos = this.activatedRoute.snapshot.data['photos']
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(eventTarget => this.onKeyUp(eventTarget))
-  }
-
-  ngOnDestroy () {
-    this.debounce.unsubscribe()
-  }
-
-  onKeyUp(target: EventTarget | null) {
-    if(target instanceof EventTarget) {
-      const element = target as HTMLInputElement
-      this.filter = element.value
-    }
   }
 
   load() {
