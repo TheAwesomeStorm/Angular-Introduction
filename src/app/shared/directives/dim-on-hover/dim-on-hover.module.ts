1e355d4e74de7a15d890ea5a21809a99c7aab7fa
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-11 10:49:32 -0300

    Added DimOnHover directive and module

---
+++ src/app/shared/directives/dim-on-hover/dim-on-hover.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DimOnHoverDirective } from './dim-on-hover.directive';
+
+
+
+@NgModule({
+  declarations: [
+    DimOnHoverDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DimOnHoverModule { }
