9eb3154a61ac548fb1cd0614c8dcb5f897235fb1
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-11 13:07:00 -0300

    Modified SignInComponent html, imported HomeModule to AppModule and added HomeModule as main route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { PhotosModule } from './photos/photos.module'
 import { AppRoutingModule } from './app.routing.module'
 import { ErrorsModule } from './errors/errors.module';
 import { CardComponent } from './shared/components/card/card.component'
+import { HomeModule } from './home/home.module'
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { CardComponent } from './shared/components/card/card.component'
     BrowserModule,
     PhotosModule,
     ErrorsModule,
+    HomeModule,
     AppRoutingModule
   ],
   providers: [],
