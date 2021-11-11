7e3666265800b327825860114965625cd8f91fec
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-11 10:19:50 -0300

    Added output onTyping, ngOnInit, ngOnDestroy and method onKeyUp to SearchComponent

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core'
+import { debounceTime, Subject } from 'rxjs'
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+
+  debounce: Subject<EventTarget | null> = new Subject<EventTarget | null>()
+  @Output() onTyping = new EventEmitter<string>()
 
   constructor() { }
 
   ngOnInit(): void {
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(eventTarget => this.onTyping.emit(this.onKeyUp(eventTarget)))
+  }
+
+  ngOnDestroy () {
+    this.debounce.unsubscribe()
+  }
+
+  onKeyUp(target: EventTarget | null): string {
+    if(target instanceof EventTarget) {
+      const element = target as HTMLInputElement
+      return element.value
+    } else {
+      return ''
+    }
   }
 
 }
