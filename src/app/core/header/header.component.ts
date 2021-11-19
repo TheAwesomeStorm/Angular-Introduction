39075914e0c017fd170201011bfbc55d5fa61653
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-19 13:53:38 -0300

    Added logout method to HeaderComponent and UserService

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service'
 import { Observable } from 'rxjs'
 import { User } from '../user/user'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,16 @@ export class HeaderComponent implements OnInit {
 
   user$: Observable<User>
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     this.user$ = userService.getUser() as Observable<User>
   }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.userService.logout()
+    this.router.navigate(['']).then(r => console.log(r))
   }
 
 }
