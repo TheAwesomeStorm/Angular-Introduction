aeb51e808ceeccba37e8ccc6b1c0f2d03d79e1f6
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-19 11:43:28 -0300

    Added UserService, AuthService now constructs UserService instead of TokenService

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { tap } from 'rxjs/operators'
-import { TokenService } from '../token/token-service'
+import { UserService } from '../user/user.service'
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +10,14 @@ export class AuthService {
 
   private apiUrl = 'http://localhost:3000'
 
-  constructor(private  http: HttpClient, private tokenService: TokenService) { }
+  constructor(private  http: HttpClient, private userService: UserService) { }
 
   authenticate(userName: string, password: string) {
     return this.http.post(this.apiUrl + '/user/login', {userName, password }, {observe: 'response'})
                     .pipe(tap(
                       res => {
                         const authToken = res.headers.get('x-access-token') as string
-                        this.tokenService.setToken(authToken)
+                        this.userService.setToken(authToken)
                         console.log(`User ${userName} authenticated with token ${authToken}`)
                       }
                     ))
