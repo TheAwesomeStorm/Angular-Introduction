93aeeb765078a48aecc1b36112c10f699ffae683
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-11-19 11:50:28 -0300

    Added constructor and  methods: [decodeAndNotify, getUser] to UserService

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { TokenService } from '../token/token-service'
+import { Observable, Subject } from 'rxjs'
+import { User } from './user'
+import jwtDecode from 'jwt-decode'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private tokenService: TokenService) { }
+  private userSubject = new Subject<User>()
+
+  constructor(private tokenService: TokenService) {
+    this.tokenService.hasToken() &&
+      this.decodeAndNotify()
+  }
 
   setToken(token: string) {
     this.tokenService.setToken(token)
   }
 
-  getUser(): void {}
+  private decodeAndNotify() {
+    const token = this.tokenService.getToken() as string
+    const user = jwtDecode(token) as User;
+    this.userSubject.next(user)
+  }
+
+  getUser(): Observable<User> {
+    return this.userSubject.asObservable()
+  }
 }
