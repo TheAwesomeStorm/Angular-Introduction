d4d5d7a2e615c64c1fc15dd894aeeaaccc770caf
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-12 11:48:26 -0300

    Added SignUpComponent, included at HomeModule, routing added at AppRoutingModule, added a margin to HeaderComponent

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MessageModule } from '../shared/components/message/message.module'
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component'
 
 
 
 @NgModule({
   declarations: [
-    SignInComponent
+    SignInComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule,
