cdc4cf1ab18dc5a8924f14e0235ead3e71180820
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-12 12:01:30 -0300

    Added FormGroup and FormControl to SignUpComponent

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public signupForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {
+    this.signupForm = new FormGroup({
+      email: new FormControl(''),
+      userName: new FormControl(''),
+      fullName: new FormControl(''),
+      password: new FormControl('')
+    })
+  }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ]],
+      fullName: ['',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]],
+      password: ['',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(14)
+      ]]
+    })
   }
 
 }
